e8f182129446023cf53d32e5ea24ef7ae7aec1a8
xh卷心菜 <15144050+xh-cabbage@user.noreply.gitee.com>
2025-09-12 08:52:32 +0800

    first

---
+++ src/components/Widget/focus.vue
@@ -0,0 +1,10 @@
+<!-- 创建时间：2025-09-12 08:32 -->
+<!-- 功能： -->
+<!-- 文件名：focus.vue -->
+<script setup lang="ts"></script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,11 @@
+<!-- 创建时间：2025-09-12 08:28 -->
+<!-- 功能： -->
+<!-- 文件名：index.vue -->
+<script setup lang="ts"></script>
+
+<template>
+  <div>111</div>
+  <focus />
+</template>
+
+<style scoped></style>
